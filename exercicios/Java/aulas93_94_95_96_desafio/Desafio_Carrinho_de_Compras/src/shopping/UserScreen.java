5fd8f35ed12a96f156da3fe37b9a00c9ba4c6a23
Isaias <99995191+isaias30silva@users.noreply.github.com>
2022-09-20 18:20:14 -0300

    Update UserScreen.java

--- exercicios/Java/aulas93_94_95_96_desafio/Desafio_Carrinho_de_Compras/src/shopping/UserScreen.java
+++ exercicios/Java/aulas93_94_95_96_desafio/Desafio_Carrinho_de_Compras/src/shopping/UserScreen.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class UserScreen implements UserInterface {
     
     Cart cart = new Cart();
+
+    List<Product> products = new Products().getProducts();
+    
     private int opt = 0;
 
     //método construtor
@@ -17,6 +20,9 @@ public class UserScreen implements UserInterface {
     public void startScreen() {
         System.out.println("1 - Menu de produtos");
         System.out.println("2 - Itens do carrinho");
+        System.out.println("3 - Quantidade de itens no carrinho");
+        System.out.println("4 - Listar preços do carrinho");
+        System.out.println("5 - Preço total do carrinho");
         System.out.println("0 - Sair");
     }
 
@@ -38,20 +44,31 @@ public class UserScreen implements UserInterface {
 
     public void displayProducts() {
         
-        List<Product> product = new Products().getProducts();
-        for(Product prod: product) {
+        for(Product prod: products) {
             System.out.println("Id do produto " + prod.getId());
             System.out.println("Nome do produto " + prod.getName());
             System.out.println("Marca do produto " + prod.getBrand());
             System.out.println("Descrição do produto " + prod.getDescription());
             System.out.println("Preço do produto " + prod.getPrice());
+
+            System.out.println();
         }
     }
 
     //método para adicionar um produto ao carrinho pelo ID
     public void addProductToCart() {
         int id = getUserInput();
-        cart.addProduct(id);
+        
+        try {
+            if(id >= products.size()) {
+                throw new ProductNotFoundException("Produto não encontrado");
+            } else {
+                cart.addProduct(id);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     //método para apresentar ao cliente carrinho com os produtos adicionados
@@ -61,22 +78,47 @@ public class UserScreen implements UserInterface {
         cart.printCart();
     }
 
+    //método para apresentar ao cliente a quantidade de itens do carrinho
+    private void showCartQuantity() {
+        System.out.println("Total de itens no carrinho");
+        cart.itemsCount();
+    }
+
+    //método para apresentar ao cliente o preço total dos itens do carrinho
+    private void showCartTotalPrice() {
+        System.out.println("Valor total do carrinho");
+        cart.getCartTotalPrice();
+    }
+
+    //Método para apresentar ao clientes os preços de cada item do carrinho
+    private void showCartPrices() {
+        System.out.println("Lista de preços do carrinho");
+        cart.listCartPrices();
+    }
+
+    //Método que permite a exclusão de um item do carrinho
     public void removeProductFromCart() {
         int id = getUserInput();
-        System.out.println("Removendo produtos com o ID" + id);
         cart.removeProduct(id);
+        System.out.println("Removendo produtos com o ID" + id);
     }
 
     //menu de produtos para adicionar ou remover do carrinho
     public void subMenu() {
-        switch(opt) {
-            case 1:
-                addProductToCart();
-                showCart();
-                break;
-            case 2:
-                removeProductFromCart();
-                break;
+        try {
+            switch(opt) {
+                case 1:
+                    addProductToCart();
+                    showCart();
+                    break;
+                case 2:
+                    removeProductFromCart();
+                    break;
+                default:
+                    throw new OptionException("Opção inválida");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
     
@@ -87,33 +129,37 @@ public class UserScreen implements UserInterface {
             startScreen();
             //pega informação do teclado
             getUserInput();
-            switch(opt) {
-                case 1:
-                    displayProducts();
-                    productMenu();
-                    getUserInput();
-                    subMenu();
-                    break;
-                case 2:
-                //mostra a lista de produtos no carrinho
-                    showCart();
-                    break;
-                case 3:
-                //mostra a quantidade de produtos no carrinho
-                    itemsCount();
-                    break;
-                case 4:
-                //mostra os preços dos produtos no carrinho
-                    listCartPrices();
-                    break;
-                case 5:
-                //mostra o total de compras do carrinho
-                    getCartTotalPrice();
-                    break;
-                case 0:
-                    System.out.println("Bye!");
-                    System.exit(0);
-                    break;
+            try {
+                switch(opt) {
+                    case 1:
+                        displayProducts();
+                        productMenu();
+                        getUserInput();
+                        subMenu();
+                        break;
+                    case 2:
+                    //mostra a lista de produtos no carrinho
+                        showCart();
+                        break;
+                    case 3:
+                    //mostra a quantidade de produtos no carrinho
+                        showCartQuantity();
+                        break;
+                    case 4:
+                    //mostra os preços dos produtos no carrinho
+                        showCartPrices();
+                        break;
+                    case 5:
+                    //mostra o total de compras do carrinho
+                        showCartTotalPrice();
+                        break;
+                    case 0:
+                        System.out.println("Bye!");
+                        System.exit(0);
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
         } while(opt !=0);
     }
